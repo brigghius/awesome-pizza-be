9f6e31519dbe6e81404d8a8669483edfa1ee82d1
Alemanno Fabrizio <alemanno.fabrizio@gmail.com>
2026-05-15 14:47:40 +0200

    fix cors block

--- src/main/java/it/alemanno/awesome_pizza/controller/OrderController.java
+++ src/main/java/it/alemanno/awesome_pizza/controller/OrderController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class OrderController {
     private final OrderService orderService;
 
